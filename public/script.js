11b2316f52724d983ac0db6d3bef20f181b714ab
unknown <contact@workin.co.il>
2021-08-09 12:00:02 +0300

    login-logout

--- public/script.js
+++ public/script.js
@@ -11,13 +11,43 @@ document.querySelector('form').onsubmit = event => {
     login(values.email, values.password)
 }
 
-let user
 async function login(email, password) {
+    let user
     const res = await axios.post('/login', { email, password })
 
     user = res.data
     console.log(user)
 
+    localStorage.token = user.token
+
     document.querySelector('main')
         .innerHTML = `<h1>Hello ${user.name}</h1>`
+}
+
+if (localStorage.token) {
+    document.querySelector('main')
+        .innerHTML = `
+        <h1>connected</h1>
+        <button onclick='logout()'>logout</button>
+        `
+}
+
+async function getUsers() {
+    try {
+        const res = await axios.get('/users', {
+            headers: { authorization: localStorage.token }
+        })
+
+        console.log(res)
+
+    } catch (error) {
+        console.log('לא מורשה')
+    }
+}
+getUsers()
+
+
+function logout() {
+    delete localStorage.token
+    location.reload()
 }
